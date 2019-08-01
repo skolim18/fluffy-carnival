90564dcae32874b60b113c7878febe7ce39c6f3b
skolim18 <skolim18@gmail.com>
2019-08-01 18:27:56 +0200

    Third

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+
+
+const app = express();
+
+app.use((req, res, next) => res.status(404).send('Cannot connect to DataBase!'));
+mongoose.connect(config.url)
+    .then(
+        () => db.seedDb(
+            () => app.listen(9090,
+                () => console.log('Server started on PORT: 9090'))
+        )
+    )
